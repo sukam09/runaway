aefae0d265f45f03da76961979949804a3b9a692
sukam09 <sukam09@naver.com>
2020-11-08 02:14:23 +0900

    Edit main process to function

--- src/main.py
+++ src/main.py
@@ -3,25 +3,29 @@ from parsing import parsing
 from match_preprocessing import match_preprocessing
 
 
-# API key
-api_key_file = open('../api_key.txt', 'r')
-API_KEY = api_key_file.read()
+def main():
+    # API key
+    api_key_file = open('../api_key.txt', 'r')
+    API_KEY = api_key_file.read()
 
-# Number of recent matches (Default: 20)
-MATCHES = 20
-SEASON = 2020
+    # Number of recent matches (Default: 20)
+    MATCHES = 20
+    SEASON = 2020
 
 
-print('\nRunaway\n')
-print('리그 오브 레전드 승패 예측을 이용한 닷지 추천 시스템')
-print('League of Legends Queue Dodging Recommendation System using Win-Loss Prediction')
-print('(c) 2020 Lee Seung Won. All rights reserved.')
+    print('\nRunaway\n')
+    print('리그 오브 레전드 승패 예측을 이용한 닷지 추천 시스템')
+    print('League of Legends Queue Dodging Recommendation System using Win-Loss Prediction')
+    print('(c) 2020 Lee Seung Won. All rights reserved.')
 
-# Parsing summoner names from in-game chat
-participant_list = parsing(API_KEY)
-for summoner_idx in range(5):
-    summoner_name = participant_list[summoner_idx]
-    print()
-    match_preprocessing(API_KEY, summoner_name, summoner_idx, summoner_spell, champion, item, rune)
+    # Parsing summoner names from in-game chat
+    participant_list = parsing(API_KEY)
+    for summoner_idx in range(5):
+        summoner_name = participant_list[summoner_idx]
+        print()
+        match_preprocessing(API_KEY, summoner_name, summoner_idx, summoner_spell, champion, item, rune)
 
-print('\n모든 소환사의 전적 전처리가 완료되었습니다.')
+    print('\n모든 소환사의 전적 전처리가 완료되었습니다.')
+
+if __name__ == '__main__':
+    main()
